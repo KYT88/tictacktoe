fb34e954d728ca2faf5eb3155084ebbc5d259497
KYT88 <158803821+KYT88@users.noreply.github.com>
2024-11-26 12:13:13 +0100

    Add files via upload

---
+++ MarkType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tiktaktoe
+{
+    /// <summary>
+    /// the type of valeu a cell in a the game is currently at
+    /// </summary>
+
+    public enum MarkType
+    {
+        /// <summary>
+        /// the cell hasn't been clicked yet
+        /// </summary>
+        Free,
+        /// <summary>
+        /// The cell is a O
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// The cell is an X
+        /// </summary>
+        Cross
+    }
+}
